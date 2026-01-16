7ad5027628855d4c160d129b1b23de7ba26f00f4
GustavoLucianoDev <147885701+GustavoLucianoDev@users.noreply.github.com>
2026-01-16 04:14:06 -0300

    Add files via upload

---
+++ public/historico/historico.js
@@ -0,0 +1,93 @@
+const MAX_TIME = 20000; // 20s
+const start = Date.now();
+let fired = false;
+isFirstTime = true;
+
+function isFlutterViewReady(el) {
+  if (!el) return false;
+  if (isFirstTime) {
+    el.style.width = "413px";
+    el.style.height = "895px";
+    isFirstTime = false;
+  }
+
+  const style = window.getComputedStyle(el);
+
+  const visible =
+    style.display !== "none" &&
+    style.visibility !== "hidden" &&
+    parseFloat(style.opacity) !== 0;
+
+  const positioned = style.position === "absolute" && style.inset === "0px";
+
+  const sized = style.width === "414px" && style.height === "896px";
+
+  return visible && positioned && sized;
+}
+
+function check() {
+  const flutterView = document.querySelector("flutter-view");
+
+  if (isFlutterViewReady(flutterView)) {
+    fired = true;
+    observer.disconnect();
+    clearInterval(interval);
+
+    const div = document.createElement("div");
+    div.className = "historico-icon-menu";
+    div.onclick = () => ativarUIHistorico();
+
+    document.body.appendChild(div);
+  }
+}
+
+// Observa mudanças no DOM
+const observer = new MutationObserver(check);
+observer.observe(document.documentElement, {
+  childList: true,
+  subtree: true,
+  attributes: true,
+  attributeFilter: ["style", "class"],
+});
+
+// Fallback por intervalo
+const interval = setInterval(() => {
+  if (fired) return;
+
+  if (Date.now() - start > MAX_TIME) {
+    clearInterval(interval);
+    observer.disconnect();
+    console.warn("[Inject] ❌ timeout esperando flutter-view visível");
+  } else {
+    check();
+  }
+}, 200);
+
+document.addEventListener("click", (event) => {
+  const historico = document.getElementById("historico-root");
+  const historicoIconMenu = document.querySelector(".historico-icon-menu");
+
+  if (!historico && !historicoIconMenu) return;
+
+  // Se o clique NÃO foi dentro do historico-root
+  if (!historico.contains(event.target) && !historicoIconMenu.contains(event.target)) {
+    console.log("👉 Clique fora do historico");
+    
+    // ação desejada
+    historico.remove(); // ou hide
+  }
+});
+
+
+function ativarUIHistorico() {
+  if (!document.getElementById("historico-root")) {
+    fetch("/inject/historico/historico.html")
+      .then((res) => res.text())
+      .then((html) => {
+        const container = document.createElement("div");
+        container.id = "historico-root";
+        container.innerHTML = html;
+        document.body.appendChild(container);
+      });
+  }
+}
